8171cef5f3f4e3328abac402e814c4fa7824e3ff
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-10-21 20:10:23 +0400

    Description using wiki markup.

--- GoogleCodePrettify.i18n.php
+++ GoogleCodePrettify.i18n.php
@@ -2,9 +2,9 @@
 
 $messages = array();
 $messages['en'] = array(
-  'google-code-prettify-description' => 'This extension adds <b>syntaxhighlight</b> tag that implemented using <a href="http://google-code-prettify.googlecode.com/svn/trunk/README.html">Google Code Prettify</a> library.'
+  'google-code-prettify-description' => 'This extension adds **syntaxhighlight** tag that implemented using [http://google-code-prettify.googlecode.com/svn/trunk/README.html Google Code Prettify] library.'
 );
 $messages['ru'] = array(
-  'google-code-prettify-description' => 'Это расширение добавляет тэг <b>syntaxhighlight</b>, который реализован с помощью библиотеки <a href="http://google-code-prettify.googlecode.com/svn/trunk/README.html">Google Code Prettify</a>.'
+  'google-code-prettify-description' => 'Это расширение добавляет тэг **syntaxhighlight**, который реализован с помощью библиотеки [http://google-code-prettify.googlecode.com/svn/trunk/README.html Google Code Prettify].'
 );
 
